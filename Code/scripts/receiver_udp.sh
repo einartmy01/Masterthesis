132ce9f37e052887ac00a0e10279988d6a2b5f65
Einar Myhrvold <einartmy@gmail.com>
2026-02-05 15:27:09 +0100

    Nice

--- Code/scripts/receiver_udp.sh
+++ Code/scripts/receiver_udp.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Starting a gstreamer pipeline
 gst-launch-1.0 \
-#f
 udpsrc port=5000 caps="application/x-rtp,media=video,encoding-name=H264,payload=96" ! \
 rtph264depay ! \
 avdec_h264 ! \
--- Code/scripts/sender_udp.sh
+++ Code/scripts/sender_udp.sh
@@ -3,15 +3,20 @@
 #Purpose:
 #	Capture video from a V4L2 camera, encode with H.264 using Intel Quick Sync (VAAPI), 
 #	packetize into RTP, and transmit over UDP
-
 #Timing model:
 #	Timestamp origin: v4l2src (capture time)
 #	No clock synchronization or buffering at sink
+#Starts a gstreamer pipeline
+#Captures video from , including timestamp from kernel clock
+#Converts pixel format (VAAPI prefers NV12)
+#Activate hardware H.264 encoder. One I-frame per second at 30fps. Bitrate controls stable quaility.
+#IDK
+#IDK
 
-gst-launch-1.0 \ 					#Starts a gstreamer pipeline
-v4l2src do-timestamp=true ! \ 				#Captures video from , including timestamp from kernel clock
-videoconvert ! \ 					#Converts pixel format (VAAPI prefers NV12)
-vaapih264enc keyframe-period=30 bitrate=4000 ! \ 	#Activate hardware H.264 encoder. One I-frame per second at 30fps. Bitrate controls stable quaility.
-rtph264pay config-interval=1 pt=96 ! \			#
-udpsink host=127.0.0.1 port=5000 sync=false async=false	#
+gst-launch-1.0 \
+v4l2src device=/dev/video0 do-timestamp=true ! \
+videoconvert ! \
+x264enc tune=zerolatency speed-preset=ultrafast bitrate=4000 key-int-max=30 ! \
+rtph264pay config-interval=1 pt=96 ! \
+udpsink host=127.0.0.1 port=5000 sync=false async=false	
 
