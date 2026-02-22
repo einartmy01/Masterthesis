2e5fd8057f126a9fc62a1e9cded8366a7378bf30
Einar Myhrvold <einartmy@gmail.com>
2026-02-22 13:26:34 +0100

    sender

--- Code/scripts/sender_udp.sh
+++ Code/scripts/sender_udp.sh
@@ -11,13 +11,15 @@
 #Converts pixel format (VAAPI prefers NV12)
 #Activate hardware H.264 encoder. One I-frame per second at 30fps. Bitrate controls stable quaility.
 #IDK
-#IDK
+#IDKgrep "time=(guint64)" > sender_log.txt
 #Swap internal host=127.0.0.1 
 
+GST_TRACERS="latency" \
+GST_DEBUG="GST_TRACER:7" \
 gst-launch-1.0 \
 v4l2src device=/dev/video0 do-timestamp=true ! \
 videoconvert ! \
-x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=4000 ! \
-rtph264pay config-interval=1 pt=96 ! \
-udpsink host=10.22.93.21 port=5000 sync=false async=false
-
+x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=4000 bframes= 0 ! \
+rtph264pay pt=96 ! \
+udpsink host=10.194.81.249 port=5000 sync=false async=false \
+2>&1 | grep "time=(guint64)" > sender_log.txt
