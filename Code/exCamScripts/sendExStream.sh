db4b85148e0498696167fb52bd15ef60a268e844
einartmy01 <einartmy@gmail.com>
2026-03-03 13:46:54 +0100

    new Script

---
+++ Code/exCamScripts/sendExStream.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+CAM_IP="192.168.1.100"
+USER="admin"
+PASS="NilsNils"
+RTSP_PORT="554"
+INTERFACE="enx00e04c681e86"
+
+echo "Configuring network..."
+
+sudo ip addr flush dev $INTERFACE
+sudo ip addr add 192.168.1.10/24 dev $INTERFACE
+sudo ip link set $INTERFACE up
+
+echo "Checking camera reachability..."
+ping -c 2 $CAM_IP > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Camera not reachable."
+    exit 1
+fi
+
+echo "Checking RTSP port..."
+nc -z -w 3 $CAM_IP $RTSP_PORT
+if [ $? -ne 0 ]; then
+    echo "RTSP port closed or not reachable."
+    exit 1
+fi
+
+echo "Starting video stream..."
+
+gst-launch-1.0 \
+rtspsrc location="rtsp://$USER:$PASS@$CAM_IP:$RTSP_PORT/Streaming/Channels/101" \
+protocols=tcp latency=0 ! \
+rtph264depay ! h264parse ! avdec_h264 ! autovideosink sync=false
