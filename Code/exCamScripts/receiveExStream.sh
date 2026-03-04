e9f18cab72724d9ec684226c0494c1471fe92a1f
einartmy01 <einartmy@gmail.com>
2026-03-04 11:31:50 +0100

    receive

--- Code/exCamScripts/receiveExStream.sh
+++ Code/exCamScripts/receiveExStream.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-INTERFACE="enx00e04c681e86"
-LOCAL_IP="192.168.1.10"
 RTP_PORT="5000"
 
 echo "Configuring receiver network..."
-
-sudo ip addr flush dev $INTERFACE
-sudo ip addr add $LOCAL_IP/24 dev $INTERFACE
-sudo ip link set $INTERFACE up
-
 echo "Starting RTP receiver..."
 
 gst-launch-1.0 \
